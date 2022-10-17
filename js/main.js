3f6233626486b8a74e09ed5fcb17b01b5f1d04ad
月佬 <110339476+midnightplayer1412@users.noreply.github.com>
2022-10-18 04:06:46 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,6 +2,15 @@
 // function to get my current age
 function getAge(dateString) {
     var ageInMilliseconds = new Date() - new Date(dateString);
-    return Math.floor(ageInMilliseconds/1000/60/60/24/365); // convert to years
+    document.getElementById('age').innerHTML = Math.floor(ageInMilliseconds/1000/60/60/24/365); // convert to years
  }
- //console.log(getAge('1999-07-13'));
+ console.log(getAge('1999-07-13'));
+
+ let swiper = new Swiper(".swiper", {
+    direction: "vertical",
+    mousewheel: true,
+        pagination: {
+          el: ".swiper-pagination",
+        },
+  });
+ 
